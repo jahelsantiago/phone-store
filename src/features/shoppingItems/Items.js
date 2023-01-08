d936a81a24f4dc905e3aa89ff3287cdbb9e70d4a
Jahel_Leon <jleonro@unal.edu.co>
2023-01-07 20:59:03 -0500

    finished fetch API

--- src/features/shoppingItems/Items.js
+++ src/features/shoppingItems/Items.js
@@ -1,12 +1,16 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { selectItems } from "./itemsSlice";
+import { fetchItems, selectItems } from "./itemsSlice";
 import ProductTypes from "./ProductTypes";
 import Product from "./Product";
 
 export const Items = (props) => {
-  const { products } = props;
+  const { products, fetchProducts } = props;
+  
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
 
   return (
     <div className="w-full  bg-blue-gray-100 gap-4 p-3 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
@@ -25,6 +29,8 @@ const mapStateToProps = (state) => ({
   products: selectItems(state),
 });
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = (dispatch) => ({
+  fetchProducts: () => dispatch(fetchItems()),
+  });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Items);
